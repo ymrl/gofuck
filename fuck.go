d8b798ae459ff9547e82177c344d1857e2c4ff86
ymrl <ymrl@ymrl.net>
2018-02-16 09:24:53 +0900

    Split step method from main

--- fuck.go
+++ fuck.go
@@ -24,67 +24,73 @@ func main() {
 	var pointer int = 0
 	var memory []int
 	for length, index := len(code), 0; index < length; index++ {
-		if pointer >= len(memory) {
-			memory = append(memory, make([]int, pointer-len(memory)+1)...)
-		}
-		op := code[index]
-		switch op {
-		case ">":
-			pointer++
-		case "<":
-			pointer--
-			if pointer < 0 {
-				fmt.Fprint(os.Stderr, "Error: Pointer goes negative\n")
-				os.Exit(2)
-			}
-		case "+":
-			memory[pointer] = (memory[pointer] + 1) & 0xff
-		case "-":
-			memory[pointer] = (memory[pointer] - 1) & 0xff
-		case "[":
-			if memory[pointer] == 0 {
-				count := 0
-				index++
-				for ; ; index++ {
-					if index >= length {
-						fmt.Fprint(os.Stderr, "Error: ] Not found\n")
-						os.Exit(2)
-					} else if code[index] == "]" {
-						if count == 0 {
-							break
-						} else {
-							count--
-						}
-					} else if code[index] == "[" {
-						count++
-					}
-				}
-			}
-		case "]":
-			if memory[pointer] != 0 {
-				count := 0
-				index--
-				for ; ; index-- {
-					if index < 0 {
-						fmt.Fprint(os.Stderr, "Error: [ Not found\n")
-						os.Exit(2)
-					} else if code[index] == "[" {
-						if count == 0 {
-							break
-						} else {
-							count--
-						}
-					} else if code[index] == "]" {
-						count++
-					}
-				}
-			}
-		case ".":
-			fmt.Print(string(memory[pointer]))
-		case ",":
-			reader := bufio.NewReader(os.Stdin)
-			input, _ := reader.ReadString('\n')
-			memory[pointer] = int(input[0]) & 0xff
-		}
+		index, pointer, memory = step(code, index, pointer, memory)
 	}
+}
+
+func step(code []string, index int, pointer int, memory []int) (int, int, []int) {
+	if pointer >= len(memory) {
+		memory = append(memory, make([]int, pointer-len(memory)+1)...)
+	}
+	length := len(code)
+	op := code[index]
+	switch op {
+	case ">":
+		pointer++
+	case "<":
+		pointer--
+		if pointer < 0 {
+			fmt.Fprint(os.Stderr, "Error: Pointer goes negative\n")
+			os.Exit(2)
+		}
+	case "+":
+		memory[pointer] = (memory[pointer] + 1) & 0xff
+	case "-":
+		memory[pointer] = (memory[pointer] - 1) & 0xff
+	case "[":
+		if memory[pointer] == 0 {
+			count := 0
+			index++
+			for ; ; index++ {
+				if index >= length {
+					fmt.Fprint(os.Stderr, "Error: ] Not found\n")
+					os.Exit(2)
+				} else if code[index] == "]" {
+					if count == 0 {
+						break
+					} else {
+						count--
+					}
+				} else if code[index] == "[" {
+					count++
+				}
+			}
+		}
+	case "]":
+		if memory[pointer] != 0 {
+			count := 0
+			index--
+			for ; ; index-- {
+				if index < 0 {
+					fmt.Fprint(os.Stderr, "Error: [ Not found\n")
+					os.Exit(2)
+				} else if code[index] == "[" {
+					if count == 0 {
+						break
+					} else {
+						count--
+					}
+				} else if code[index] == "]" {
+					count++
+				}
+			}
+		}
+	case ".":
+		fmt.Print(string(memory[pointer]))
+	case ",":
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		memory[pointer] = int(input[0]) & 0xff
+	}
+	return index, pointer, memory
 }
